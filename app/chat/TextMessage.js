2fd90d215132487bff64a439f53b27e52b3202ff
boboxiaodd <iam386@gmail.com>
2023-03-28 13:57:37 +0800

    调整消息透明度和点击事件

--- app/chat/TextMessage.js
+++ app/chat/TextMessage.js
@@ -1,121 +1,76 @@
-import React, { PureComponent } from 'react'
+import React, {PureComponent} from 'react';
 import {
-  View,
-  TouchableOpacity,
-  ActivityIndicator,
-  Platform,
-  StyleSheet,
-  Dimensions,
-  Text
-} from 'react-native'
+    View, TouchableOpacity, ActivityIndicator, Platform, StyleSheet, Dimensions, Text,
+} from 'react-native';
 import {PressableOpacity} from 'react-native-pressable-opacity';
-import { changeEmojiText } from './utils'
-const { width } = Dimensions.get('window')
+import {changeEmojiText} from './utils';
+
+const {width} = Dimensions.get('window');
 
 export default class TextMessage extends PureComponent {
-  render () {
-    const { isSelf, message, messageErrorIcon, views, isOpen, rightMessageBackground, leftMessageBackground, reSendMessage, chatType, isReadStyle, showIsRead, ImageComponent } = this.props
-    return (
-      <View
-        style={[isSelf ? styles.right : styles.left]}
-        collapsable={false}
-        ref={(e) => (this[`item_${this.props.rowId}`] = e)}
-      >
-        <View
-          style={
-            [
-              styles.triangle,
-              isSelf
-                ? styles.right_triangle
-                : styles.left_triangle,
-              { borderColor: isSelf ? rightMessageBackground : leftMessageBackground }
-            ]}
-        />
-        <PressableOpacity
-          disabled={isOpen}
-          onLongPress={() => {
-            this.props.onMessageLongPress(message.type, parseInt(this.props.rowId), message)
-          }}
-          onPress={() => {
-            this.props.onMessagePress(message.type, parseInt(this.props.rowId),  message)
-          }}
-        >
-          <View style={[styles.container, { backgroundColor: isSelf ? rightMessageBackground
-              : leftMessageBackground }]}>
-            {views}
-          </View>
-          {chatType !== 'group' && isSelf && showIsRead && (
-            <Text style={[{ textAlign: 'right', fontSize: 13 }, isReadStyle]}>
-              {this.props.lastReadAt && this.props.lastReadAt - message.time > 0 ? '已读' : '未读'}
-            </Text>
-          )}
-        </PressableOpacity>
-        <View style={{ alignItems: 'center', justifyContent: 'center', marginRight: 10 }}>
-          {!isSelf
-            ? null
-            : message.sendStatus === undefined
-              ? null
-              : message.sendStatus === 0
-                ? <ActivityIndicator />
-                : message.sendStatus < 0
-                  ? <PressableOpacity
-                    disabled={false}
-                    onPress={() => {
-                      if (message.sendStatus === -2) {
-                        reSendMessage(message)
-                      }
-                    }}>
-                    {messageErrorIcon ? messageErrorIcon : <ImageComponent source={require('../source/image/waring.png')} style={{ width: 20, height: 20 }} />}
-                  </PressableOpacity>
-                  : null
-          }
-        </View>
-      </View>
-    )
-  }
+    render() {
+        const {isSelf, message, messageErrorIcon, views, isOpen, rightMessageBackground, leftMessageBackground, reSendMessage, chatType, isReadStyle, showIsRead, ImageComponent} = this.props;
+        return (<View
+                style={[isSelf ? styles.right : styles.left]}
+                collapsable={false}
+                ref={(e) => (this[`item_${this.props.rowId}`] = e)}
+            >
+                <View
+                    // activeOpacity={0.8}
+                    // disabled={isOpen}
+                    // onLongPress={() => {
+                    //     this.props.onMessageLongPress(message.type, parseInt(this.props.rowId), message);
+                    // }}
+                    // onPress={() => {
+                    //     this.props.onMessagePress(message.type, parseInt(this.props.rowId), message);
+                    // }}
+                    style={{flexDirection:'row'}}
+                >
+                    {!isSelf && <View style={[styles.triangle, styles.left_triangle, {borderColor: leftMessageBackground}]} />}
+                    <View style={[styles.container, {
+                        backgroundColor: isSelf ? rightMessageBackground : leftMessageBackground,
+                    }]}>
+                        {views}
+                    </View>
+                    {isSelf && <View style={[styles.triangle, styles.right_triangle, {borderColor: rightMessageBackground}]} />}
+                    {chatType !== 'group' && isSelf && showIsRead && (
+                        <Text style={[{textAlign: 'right', fontSize: 13}, isReadStyle]}>
+                            {this.props.lastReadAt && this.props.lastReadAt - message.time > 0 ? '已读' : '未读'}
+                        </Text>)}
+                </View>
+                <View style={{alignItems: 'center', justifyContent: 'center', marginRight: 10}}>
+                    {!isSelf ? null : message.sendStatus === undefined ? null : message.sendStatus === 0 ?
+                        <ActivityIndicator/> : message.sendStatus < 0 ? <PressableOpacity
+                            disabled={false}
+                            onPress={() => {
+                                if (message.sendStatus === -2) {
+                                    reSendMessage(message);
+                                }
+                            }}>
+                            {messageErrorIcon ? messageErrorIcon :
+                                <ImageComponent source={require('../source/image/waring.png')} style={{width: 20, height: 20}}/>}
+                        </PressableOpacity> : null}
+                </View>
+            </View>);
+    }
 }
 const styles = StyleSheet.create({
 
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    flexWrap: 'wrap',
-    borderRadius: 12,
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    maxWidth: width - 160,
-    minHeight: 20,
-  },
+    container: {
+        flexDirection: 'row', alignItems: 'center', flexWrap: 'wrap', borderRadius: 12, paddingHorizontal: 15, paddingVertical: 15, maxWidth: width - 160, minHeight: 20,
+    },
 
-  subEmojiStyle: {
-    width: 25,
-    height: 25
-  },
-  triangle: {
-    width: 0,
-    height: 0,
-    zIndex: 999,
-    borderWidth: 6,
-    borderTopColor: 'transparent',
-    borderBottomColor: 'transparent',
-    borderColor: '#fff',
-    marginTop: 16
-  },
-  left_triangle: {
-    borderLeftWidth: 0,
-    borderRightWidth: Platform.OS === 'android' ? 6 : 10,
-    marginLeft: 5
-  },
-  right_triangle: {
-    borderRightWidth: 0,
-    borderLeftWidth: Platform.OS === 'android' ? 6 : 10,
-    borderColor: '#a0e75a',
-    marginRight: 5
-  },
-  right: {
-    flexDirection: 'row-reverse',
-  },
-  left: {
-    flexDirection: 'row',
-  }
-})
+    subEmojiStyle: {
+        width: 25, height: 25,
+    }, triangle: {
+        width: 0, height: 0, zIndex: 999, borderWidth: 6, borderTopColor: 'transparent', borderBottomColor: 'transparent', borderColor: '#fff', marginTop: 16,
+    }, left_triangle: {
+        borderLeftWidth: 0, borderRightWidth: Platform.OS === 'android' ? 6 : 10, marginLeft: 5,
+    }, right_triangle: {
+        borderRightWidth: 0, borderLeftWidth: Platform.OS === 'android' ? 6 : 10, borderColor: '#a0e75a', marginRight: 5,
+    }, right: {
+        flexDirection: 'row-reverse',
+    }, left: {
+        flexDirection: 'row',
+    },
+});
--- app/chat/VoiceMessage.js
+++ app/chat/VoiceMessage.js
@@ -1,217 +1,150 @@
-import React, { PureComponent } from 'react'
+import React, {PureComponent} from 'react';
 import {
-  View,
-  TouchableOpacity,
-  StyleSheet,
-  Text,
-  ActivityIndicator, Dimensions, Platform
-} from 'react-native'
+    View, TouchableOpacity, StyleSheet, Text, ActivityIndicator, Dimensions, Platform,
+} from 'react-native';
 import {PressableOpacity} from 'react-native-pressable-opacity';
-const { width } = Dimensions.get('window')
+
+const {width} = Dimensions.get('window');
 
 export default class VoiceMessage extends PureComponent {
-  constructor (props) {
-    super(props)
-    this.playTime = null
-    this.state = {
-      loading: false,
-      progress: 2
+    constructor(props) {
+        super(props);
+        this.playTime = null;
+        this.state = {
+            loading: false, progress: 2,
+        };
     }
-  }
 
-  UNSAFE_componentWillReceiveProps (next) {
-    if (next.pressIndex === next.rowId) {
-      this.setState({ loading: next.voiceLoading })
-      if (next.voicePlaying) {
-        this._play()
-      } else {
-        this.playTime && clearInterval(this.playTime)
-        this.setState({ progress: 2 })
-      }
-    } else {
-      this.setState({ loading: false, progress: 2 })
-      this.playTime && clearInterval(this.playTime)
-    }
-  }
-
-  _play () {
-    this.playTime && clearInterval(this.playTime)
-    let index = 0
-    const { progress } = this.state
-    if (progress === 2) index = 2
-    this.playTime = setInterval(() => {
-      if (index === 2) {
-        index = -1
-      }
-      index += 1
-      this.setState({ progress: index })
-    }, 400)
-  }
-
-  _renderIcon = () => {
-    const { isSelf, voiceLeftIcon, voiceRightIcon, ImageComponent } = this.props
-    const { progress } = this.state
-    if (isSelf) {
-      if (voiceRightIcon) {
-        return voiceRightIcon
-      } else {
-        return <ImageComponent
-          source={
-            progress === 0
-              ? require('../source/image/voiceRightOne.png')
-              : progress === 1
-              ? require('../source/image/voiceRightTwo.png')
-              : require('../source/image/voiceRight.png')
-          }
-          resizeMode={'cover'}
-          style={{
-            width: 26, height: 26
-          }} />
-      }
-    } else {
-      if (voiceLeftIcon) {
-        return voiceLeftIcon
-      } else {
-        return <ImageComponent source={progress === 0 ? require('../source/image/voiceLeftOne.png') : progress === 1 ? require('../source/image/voiceLeftTwo.png') : require('../source/image/voiceLeft.png')} resizeMode={'cover'} style={{
-          width: 26, height: 26
-        }} />
-      }
-    }
-  }
-
-  componentWillUnmount () {
-    this.playTime && clearInterval(this.playTime)
-  }
-
-  render () {
-    const { message, messageErrorIcon, isSelf, isOpen, reSendMessage, leftMessageBackground, rightMessageBackground, voiceRightLoadingColor, voiceLeftLoadingColor, chatType, isReadStyle, showIsRead} = this.props
-    const { loading } = this.state
-    return (
-      <View>
-        <View style={[isSelf ? styles.right : styles.left]}>
-          <View
-            style={
-              [
-                styles.triangle,
-                isSelf
-                  ? styles.right_triangle
-                  : styles.left_triangle,
-                loading ? { borderColor: isSelf ? voiceRightLoadingColor : voiceLeftLoadingColor } : { borderColor: isSelf ? rightMessageBackground : leftMessageBackground }
-              ]}
-          />
-          <View
-            style={{ flexDirection: isSelf ? 'row-reverse' : 'row' }}
-            collapsable={false}
-            ref={(e) => (this[`item_${this.props.rowId}`] = e)}
-          >
-            <View>
-              <PressableOpacity
-                disabled={isOpen}
-                style={
-                  [styles.voiceArea,
-                    loading
-                      ? {
-                        backgroundColor: isSelf
-                          ? voiceRightLoadingColor
-                          : voiceLeftLoadingColor
-                      }
-                      : {
-                        backgroundColor: isSelf
-                          ? rightMessageBackground
-                          : leftMessageBackground
-                      }
-                  ]
-                }
-                onPress={() => {
-                  this.props.savePressIndex(this.props.rowId)
-                  this.props.onMessagePress('voice', parseInt(this.props.rowId), message.content.uri, message)
-                }
-                }
-                onLongPress={() => {
-                  this.props.onMessageLongPress(this[`item_${this.props.rowId}`], 'voice', parseInt(this.props.rowId), message.content.uri, message)
-                }}
-              >
-                <View style={[{ width: 60 + (message.content.length > 1 ? message.content.length * 5 : 0) },{alignItems:'center'}, { maxWidth: width - 160 }, { flexDirection: isSelf ? 'row-reverse' : 'row' }
-                ]}>
-                  {!isSelf &&
-                  <Text style={[{marginLeft: 8,marginRight: 2}]}>
-                    {`${message.content.length}"`}
-                  </Text>
-                  }
-                  {this._renderIcon()}
-                  {isSelf &&
-                  <Text style={[{marginRight: 4}]}>
-                    {`${message.content.length}"`}
-                  </Text>
-                  }
-                </View>
-              </PressableOpacity>
-            </View>
-          </View>
-          <View style={{ alignItems: 'center', justifyContent: 'center', marginRight: 10 }}>
-            {!isSelf
-              ? null
-              : message.sendStatus === undefined
-                ? null
-                : message.sendStatus === 0
-                  ? <ActivityIndicator />
-                  : message.sendStatus < 0
-                    ? <PressableOpacity
-                      disabled={false}
-                      onPress={() => {
-                        if (message.sendStatus === -2) {
-                          reSendMessage(message)
-                        }
-                      }}>
-                      {messageErrorIcon}
-                    </PressableOpacity>
-                    : null
+    UNSAFE_componentWillReceiveProps(next) {
+        if (next.pressIndex === next.rowId) {
+            this.setState({loading: next.voiceLoading});
+            if (next.voicePlaying) {
+                this._play();
+            } else {
+                this.playTime && clearInterval(this.playTime);
+                this.setState({progress: 2});
             }
-          </View>
-        </View>
-        {chatType !== 'group' && isSelf && showIsRead && (
-          <Text style={[{ textAlign: 'right', fontSize: 13, marginRight: 12 }, isReadStyle]}>
-            {this.props.lastReadAt && this.props.lastReadAt - message.time > 0 ? '已读' : '未读'}
-          </Text>
-        )}
-      </View>
-    )
-  }
+        } else {
+            this.setState({loading: false, progress: 2});
+            this.playTime && clearInterval(this.playTime);
+        }
+    }
+
+    _play() {
+        this.playTime && clearInterval(this.playTime);
+        let index = 0;
+        const {progress} = this.state;
+        if (progress === 2) index = 2;
+        this.playTime = setInterval(() => {
+            if (index === 2) {
+                index = -1;
+            }
+            index += 1;
+            this.setState({progress: index});
+        }, 400);
+    }
+
+    _renderIcon = () => {
+        const {isSelf, voiceLeftIcon, voiceRightIcon, ImageComponent} = this.props;
+        const {progress} = this.state;
+        if (isSelf) {
+            if (voiceRightIcon) {
+                return voiceRightIcon;
+            } else {
+                return <ImageComponent
+                    source={progress === 0 ? require('../source/image/voiceRightOne.png') : progress === 1 ? require('../source/image/voiceRightTwo.png') : require('../source/image/voiceRight.png')}
+                    resizeMode={'cover'}
+                    style={{
+                        width: 26, height: 26,
+                    }}/>;
+            }
+        } else {
+            if (voiceLeftIcon) {
+                return voiceLeftIcon;
+            } else {
+                return <ImageComponent source={progress === 0 ? require('../source/image/voiceLeftOne.png') : progress === 1 ? require('../source/image/voiceLeftTwo.png') : require('../source/image/voiceLeft.png')} resizeMode={'cover'} style={{
+                    width: 26, height: 26,
+                }}/>;
+            }
+        }
+    };
+
+    componentWillUnmount() {
+        this.playTime && clearInterval(this.playTime);
+    }
+
+    render() {
+        const {message, messageErrorIcon, isSelf, isOpen, reSendMessage, leftMessageBackground, rightMessageBackground, voiceRightLoadingColor, voiceLeftLoadingColor, chatType, isReadStyle, showIsRead} = this.props;
+        const {loading} = this.state;
+        return (<View>
+            <View style={[isSelf ? styles.right : styles.left]}>
+                <PressableOpacity
+                    disabled={isOpen}
+                    activeOpacity={0.8}
+                    style={{flexDirection: 'row',alignItems:'center'}}
+                    collapsable={false}
+                    ref={(e) => (this[`item_${this.props.rowId}`] = e)}
+                    onPress={() => {
+                        this.props.savePressIndex(this.props.rowId);
+                        this.props.onMessagePress('voice', parseInt(this.props.rowId), message.content.uri, message);
+                    }}
+                    onLongPress={() => {
+                        this.props.onMessageLongPress(this[`item_${this.props.rowId}`], 'voice', parseInt(this.props.rowId), message.content.uri, message);
+                    }}
+                >
+                    {!isSelf  && <View style={[styles.triangle, styles.left_triangle, loading ? {borderColor: voiceLeftLoadingColor} : {borderColor: leftMessageBackground}]}/>}
+                    <View
+                        style={[styles.voiceArea, loading ? {
+                            backgroundColor: isSelf ? voiceRightLoadingColor : voiceLeftLoadingColor,
+                        } : {
+                            backgroundColor: isSelf ? rightMessageBackground : leftMessageBackground,
+                        }, {flexDirection:'row'}]}
+                    >
+                        <View style={[{width: 60 + (message.content.length > 1 ? message.content.length * 5 : 0)}, {alignItems: 'center'}, {maxWidth: width - 160}, {flexDirection: isSelf ? 'row-reverse' : 'row'}]}>
+                            {!isSelf && <Text style={[{marginLeft: 8, marginRight: 2}]}>
+                                {`${message.content.length}"`}
+                            </Text>}
+                            {this._renderIcon()}
+                            {isSelf && <Text style={[{marginRight: 4}]}>
+                                {`${message.content.length}"`}
+                            </Text>}
+                        </View>
+                    </View>
+                    {isSelf && <View style={[styles.triangle, styles.right_triangle, loading ? {borderColor: voiceRightLoadingColor} : {borderColor: rightMessageBackground }]}/>}
+                </PressableOpacity>
+                <View style={{alignItems: 'center', justifyContent: 'center', marginRight: 10}}>
+                    {!isSelf ? null : message.sendStatus === undefined ? null : message.sendStatus === 0 ?
+                        <ActivityIndicator/> : message.sendStatus < 0 ? <PressableOpacity
+                            disabled={false}
+                            onPress={() => {
+                                if (message.sendStatus === -2) {
+                                    reSendMessage(message);
+                                }
+                            }}>
+                            {messageErrorIcon}
+                        </PressableOpacity> : null}
+                </View>
+            </View>
+            {chatType !== 'group' && isSelf && showIsRead && (
+                <Text style={[{textAlign: 'right', fontSize: 13, marginRight: 12}, isReadStyle]}>
+                    {this.props.lastReadAt && this.props.lastReadAt - message.time > 0 ? '已读' : '未读'}
+                </Text>)}
+        </View>);
+    }
 }
 
 const styles = StyleSheet.create({
-  triangle: {
-    width: 0,
-    height: 0,
-    zIndex: 999,
-    borderWidth: 6,
-    borderTopColor: 'transparent',
-    borderBottomColor: 'transparent',
-  },
-  left_triangle: {
-    borderLeftWidth: 0,
-    borderRightWidth: Platform.OS === 'android' ? 6 : 10,
-    marginLeft: 5
-  },
-  right_triangle: {
-    borderRightWidth: 0,
-    borderLeftWidth: Platform.OS === 'android' ? 6 : 10,
-    borderColor: '#a0e75a',
-    marginRight: 5
-  },
-  right: {
-    flexDirection: 'row-reverse',
-    alignItems: 'center',
-  },
-  left: {
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  voiceArea: {
-    paddingVertical: 8,
-    borderRadius: 12,
-    maxWidth: width - 160,
-    justifyContent: 'center',
-    minHeight: 30
-  }
-})
+    triangle: {
+        width: 0, height: 0, zIndex: 999, borderWidth: 6, borderTopColor: 'transparent', borderBottomColor: 'transparent',
+    }, left_triangle: {
+        borderLeftWidth: 0, borderRightWidth: Platform.OS === 'android' ? 6 : 10, marginLeft: 5,
+    }, right_triangle: {
+        borderRightWidth: 0, borderLeftWidth: Platform.OS === 'android' ? 6 : 10, borderColor: '#a0e75a', marginRight: 5,
+    }, right: {
+        flexDirection: 'row-reverse', alignItems: 'center',
+    }, left: {
+        flexDirection: 'row', alignItems: 'center',
+    }, voiceArea: {
+        paddingVertical: 8, borderRadius: 12, maxWidth: width - 160, justifyContent: 'center', minHeight: 30,
+    },
+});
